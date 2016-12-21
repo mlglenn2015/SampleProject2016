a354e6dc18485eb804098799640ef5ef43e98f15
Mark L. Glenn <mark.glenn@mybrighthouse.com>
2016-12-21 18:19:44 -0500

    Testing

--- common/src/main/java/prv/mark/project/common/config/CommonDataConfig.java
+++ common/src/main/java/prv/mark/project/common/config/CommonDataConfig.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
@@ -19,9 +20,6 @@ import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import prv.mark.project.common.service.impl.ApplicationMessageSource;
-import prv.mark.project.common.service.impl.ApplicationParameterSource;
 
 import javax.sql.DataSource;
 import java.util.Optional;
@@ -35,12 +33,14 @@ import java.util.Properties;
 @Configuration
 @ComponentScan(basePackages = {"prv.mark.project"})
 @EnableJpaRepositories(basePackages = {"prv.mark.project.common.repository"})
+@EnableSpringDataWebSupport
 @EnableTransactionManagement
+@EnableJpaAuditing
 @PropertySource("classpath:common.properties")
 @Profile({"local", "dev", "qatest", "staging", "production"})
 public class CommonDataConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(CommonDataConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonDataConfig.class);
 
     @Value("${spring.jpa.show-sql}")
     private String showSql;
@@ -75,7 +75,7 @@ public class CommonDataConfig {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         // EclipseLink logging.  Default to SEVERE if not set as a system property or in a property file.
         String jpaLogging = Optional.ofNullable(env.getProperty("app.jpa.logging")).orElse("SEVERE");
-        logger.info("*** JPA logging set to {} level. ***", jpaLogging);
+        LOGGER.info("*** JPA logging set to {} level. ***", jpaLogging);
 
         LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
         emfb.setDataSource(dataSource());
@@ -97,6 +97,8 @@ public class CommonDataConfig {
 
     @Bean(destroyMethod = "")
     public DataSource dataSource() {
+        LOGGER.info("Application Id:{}", applicationId);
+        LOGGER.info("Configuring applicationJndiDataSource:{}", applicationJndiDataSource);
         final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
         dsLookup.setResourceRef(true);
         return dsLookup.getDataSource(applicationJndiDataSource);
@@ -104,6 +106,7 @@ public class CommonDataConfig {
 
     @Bean
     public PlatformTransactionManager transactionManager() {
+        LOGGER.info("Returning new JtaTransactionManager...");
         return new JtaTransactionManager();
     }
 
